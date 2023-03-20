b2f8f88051d366292b39c5d8865bc60f18d924e6
Anantha Debnath <ananthadebnath103@gmail.com>
2023-03-20 21:23:09 +0600

    all doen

--- js/calculation.js
+++ js/calculation.js
@@ -2,15 +2,6 @@ document.getElementById("blogBtn").addEventListener ('click',function(){
     window.location.replace("http://127.0.0.1:5500/blog.html");
 } )
 
-
-// document.getElementById("triB").addEventListener("click",function(){
-//     getValue("triB");
-// })
-
-// document.getElementById("triClc").addEventListener('click',function(){
-//     calculateValue("triClc");
-// })
-
 const clcBtn = document.getElementsByClassName("calcuateResult");
 for(const clc of clcBtn){
     clc.addEventListener('click',function( event){
@@ -21,7 +12,7 @@ for(const clc of clcBtn){
         console.log("lol")
        const restultContainer = document.getElementById("results");
         const li = document.createElement('li');
-        li.innerText= result.toFixed(2);
+        li.innerText=  result+" cm2";
         li.classList.add("result");
         restultContainer.appendChild(li);
     })
--- js/utility.js
+++ js/utility.js
@@ -1,10 +1,25 @@
 function getValue(elemetnId){
   let textfieldData = document.getElementById(elemetnId);
-    return textfieldData.value;
+  let value = elemetnId.value;
+  if(value != 'number'){
+    alert("Please give a number.");
+    return 0;
+  }
+    else if(value >=0)
+    return value;
+    else return 0;
 }
 function calculateValue(elementId){
     if(elementId === "triClc"){
-       return  0.5 * getValue("triB") * getValue("triH")
+       return "Triangle " + (0.5 * getValue("triB") * getValue("triH")).toFixed(2);
     }
+    else if(elementId === "recClc"){
+        return "Rectangle "+ (getValue("racW") * getValue("racH")).toFixed(2);
+    }
+    else if(elementId === "ParaClc") return "Parallelogram "+ (10*12).toFixed(2);
+    else if(elementId === "rombClc") return "Rhombus "+ (0.5 * 16 * 8).toFixed(2);
+    else if(elementId === "pentClc") return "Pentagon "+(0.5 * 6 * 10).toFixed(2);
+    else if(elementId === "ElepClc") return "Ellipse" + (3.14 * 10 * 4).toFixed(2);
+    else return 0;
 }
 
