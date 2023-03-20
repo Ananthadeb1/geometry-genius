379cca09cf6e1129ae5e3ab2f61f3aba37946cd5
Anantha Debnath <ananthadebnath103@gmail.com>
2023-03-20 22:08:27 +0600

    final touch

--- js/utility.js
+++ js/utility.js
@@ -1,20 +1,15 @@
 function getValue(elemetnId){
   let textfieldData = document.getElementById(elemetnId);
-  let value = elemetnId.value;
-  if(value != 'number'){
-    alert("Please give a number.");
-    return 0;
-  }
-    else if(value >=0)
-    return value;
-    else return 0;
-}
+ let value = textfieldData.value;
+ textfieldData.value= '';
+ return value;
+    } 
 function calculateValue(elementId){
     if(elementId === "triClc"){
        return "Triangle " + (0.5 * getValue("triB") * getValue("triH")).toFixed(2);
     }
     else if(elementId === "recClc"){
-        return "Rectangle "+ (getValue("racW") * getValue("racH")).toFixed(2);
+        return "Rectangle "+ (getValue("racW") * getValue("racL")).toFixed(2);
     }
     else if(elementId === "ParaClc") return "Parallelogram "+ (10*12).toFixed(2);
     else if(elementId === "rombClc") return "Rhombus "+ (0.5 * 16 * 8).toFixed(2);
