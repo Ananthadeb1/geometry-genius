1b2adeb76eb5da3105812de7c9d33619cbca0cd8
Anantha Debnath <ananthadebnath103@gmail.com>
2023-03-20 19:37:13 +0600

    Adding to the result section done

--- js/calculation.js
+++ js/calculation.js
@@ -1,3 +1,35 @@
 document.getElementById("blogBtn").addEventListener ('click',function(){
     window.location.replace("http://127.0.0.1:5500/blog.html");
 } )
+
+
+document.getElementById("triB").addEventListener("click",function(){
+    getValue("triB");
+})
+
+// document.getElementById("triClc").addEventListener('click',function(){
+//     calculateValue("triClc");
+// })
+
+const clcBtn = document.getElementsByClassName("calcuateResult");
+for(const clc of clcBtn){
+    clc.addEventListener('click',function( event){
+            let id;
+        document.querySelectorAll('h2').forEach(e => {
+            id = e.getAttribute('id');
+            e.addEventListener('click', function() {
+              console.log('A button with ID ' + id + ' was clicked!') 
+            } );
+          });
+
+    const result = calculateValue(id);
+        console.log("lol")
+       const restultContainer = document.getElementById("results");
+        const li = document.createElement('li');
+        li.innerText= result;
+        li.classList.add("result");
+        restultContainer.appendChild(li);
+    })
+}
+
+
---
+++ js/utility.js
@@ -0,0 +1,10 @@
+function getValue(elemetnId){
+  let textfieldData = document.getElementById(elemetnId);
+    return textfieldData.value;
+}
+function calculateValue(elementId){
+    if(elementId === "triClc"){
+       return 0.5 * getValue(id) * getValue()
+    }
+}
+
