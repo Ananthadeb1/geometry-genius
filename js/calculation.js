83b9b0c69f66b979d5c5a4837e84b68bb049564e
Anantha Debnath <ananthadebnath103@gmail.com>
2023-03-20 16:27:11 +0600

    first commit

---
+++ js/calculation.js
@@ -0,0 +1,7 @@
+document.getElementById("gioVai").addEventListener ('click',function(){
+    window.location.replace("http://127.0.0.1:5500/blog.html");
+} )
+    
+document.getElementById("anotherpage").addEventListener ('click',function(){
+    window.location.replace("http://127.0.0.1:5500/index.html");
+} )
